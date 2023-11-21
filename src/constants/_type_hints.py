7ef65e4c3d74e8f7197db3f6ff0920b2e46c8fa7
Brearton, Richard (DLSLtd,RAL,LSCI) <richardbrearton@gmail.com>
2023-11-21 21:50:20 +0000

    Added real number type hint

--- src/constants/_type_hints.py
+++ src/constants/_type_hints.py
@@ -15,3 +15,8 @@ class TypeHints:
     This type hint represents any individual numeric value. This number could be an
     element of an arbitrary field.
     """
+
+    real = int | float | np.number
+    """
+    This type hint represents any real number.
+    """
