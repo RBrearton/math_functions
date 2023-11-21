f18c8f576ad1aa47a38e8ae3a79fb12aaa949a72
Brearton, Richard (DLSLtd,RAL,LSCI) <richardbrearton@gmail.com>
2023-11-21 21:09:55 +0000

    Implemented evaluate method

--- src/_real_function.py
+++ src/_real_function.py
@@ -3,6 +3,7 @@ This file contains the ContFunction class, which is used to represent a general
 continuous function.
 """
 
+from typing import Callable
 
 from .interfaces import IDifferentiationEngine, IFunction, IIntegrationEngine, IMathSet
 
@@ -12,6 +13,9 @@ class RealFunction(IFunction):
     This class represents a general continuous real function. Most functions are
     instances of RealFunction.
     """
+
+    def __init__(self, function_to_call: Callable) -> None:
+        self._function = function_to_call
 
     @property
     def integrate(self) -> IIntegrationEngine:
@@ -28,6 +32,9 @@ class RealFunction(IFunction):
     @property
     def codomain(self) -> "IMathSet":
         raise NotImplementedError()
+
+    def evaluate(self, arguments: tuple[float, ...]) -> float:
+        return self._function(*arguments)
 
     def plot(self):
         raise NotImplementedError()
