8c758cccc269dec8a272b20b0ccd533182a10d65
Brearton, Richard (DLSLtd,RAL,LSCI) <richardbrearton@gmail.com>
2023-11-21 22:13:10 +0000

    Made Open/Closed intervals public

---
+++ src/math_toolbox/sets/intervals/__init__.py
@@ -0,0 +1,6 @@
+"""
+This script defines the public interface of the math_toolbox.sets.intervals submodule.
+"""
+
+from ._closed_interval import ClosedInterval
+from ._open_interval import OpenInterval
