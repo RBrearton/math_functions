d19b65c45f613a59216e564200dce11f4a29bbea
Brearton, Richard (DLSLtd,RAL,LSCI) <richardbrearton@gmail.com>
2023-11-22 19:18:53 +0000

    Added ISeries interface

---
+++ src/math_toolbox/interfaces/_series.py
@@ -0,0 +1,59 @@
+"""
+This file contains the ISeries interface, which is used to represent a general
+mathematical series.
+"""
+
+from abc import ABC, abstractmethod
+
+from ._function import IFunction
+from ..constants import TypeHints
+
+
+class ISeries(ABC):
+    """
+    This interface represents a general mathematical series. This general ISeries
+    interface allows for terms in the series to be either numbers or functions.
+    """
+
+    @property
+    @abstractmethod
+    def does_converge(self) -> bool:
+        """
+        This method returns True if the series converges and False otherwise.
+        """
+
+    @abstractmethod
+    def compute_term(self, index: int) -> TypeHints.numeric | IFunction:
+        """
+        This method returns the value of the term at the given index. If index = n,
+        then this method returns the value of the nth term in the series.
+
+        Args:
+            index:
+                The index of the term to compute.
+
+        Returns:
+            The value of the term at the given index.
+        """
+
+    @abstractmethod
+    def compute_sum(self, number_of_terms: int) -> TypeHints.numeric | IFunction:
+        """
+        This method returns the value of the sum of the first n terms in the series.
+
+        Args:
+            number_of_terms:
+                The number of terms to sum.
+
+        Returns:
+            The value of the sum of the first n terms in the series.
+        """
+
+    @abstractmethod
+    def compute_infinite_sum(self) -> TypeHints.numeric | IFunction:
+        """
+        This method returns the value of the infinite sum of the series.
+
+        Returns:
+            The value of the infinite sum of the series.
+        """
