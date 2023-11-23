94e4d3cc7cd635c96aae263c7cd46f1a84e2c0a9
Brearton, Richard (DLSLtd,RAL,LSCI) <richardbrearton@gmail.com>
2023-11-23 18:19:23 +0000

    Made various improvements to the FunctionSeries class

--- src/math_toolbox/series/_function_series.py
+++ src/math_toolbox/series/_function_series.py
@@ -5,6 +5,7 @@ series of functions whose domains and codomains are the real numbers.
 
 from collections.abc import Callable
 
+from ..exceptions import InvalidNumberOfTermsError, InvalidTermError
 from ..interfaces import IFunction, ISeries
 
 
@@ -21,10 +22,28 @@ class FunctionSeries(ISeries):
         raise NotImplementedError()
 
     def compute_term(self, index: int) -> IFunction:
+        # Make sure that the index is positive.
+        if index <= 0:
+            raise InvalidTermError(index)
+
+        # The index is valid, so return the requested term.
         return self._function(index)
 
-    def compute_sum(self, number_of_terms: int) -> IFunction:
-        return sum(self._function(i) for i in range(number_of_terms))
+    def compute_partial_sum(self, number_of_terms: int) -> IFunction:
+        # Make sure that we were given a positive integer number of terms to sum.
+        if number_of_terms <= 0:
+            raise InvalidNumberOfTermsError(number_of_terms)
+
+        # Find the requested terms.
+        requested_terms = range(1, number_of_terms + 1)
+
+        # Compute the partial sum.
+        partial_sum = self.compute_term(1)
+        for term in requested_terms[1:]:
+            partial_sum += self.compute_term(term)
+
+        # Return the computed partial sum.
+        return partial_sum
 
     def compute_infinite_sum(self) -> IFunction:
         raise NotImplementedError()
